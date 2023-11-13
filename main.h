be0bcfa4664cdfabdedbd73d19b75fca3fd4bfa7
RowanAshour <rokamino@hotmail.com>
2023-11-13 03:22:13 -0800

    0

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <limits.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
